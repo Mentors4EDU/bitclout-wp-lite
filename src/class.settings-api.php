8ef7583d42164a0cf66654869fb3db8711c544d9
Andrew Kamal <kamalandrew55@gmail.com>
2021-08-30 23:10:01 -0400

    Update class.settings-api.php

--- src/class.settings-api.php
+++ src/class.settings-api.php
@@ -1,152 +1,152 @@
 <?php
-add_action( 'admin_menu', 'bitclout-wp-lite_add_admin_menu' );
-add_action( 'admin_init', 'bitclout-wp-lite_settings_init' );
+add_action( 'admin_menu', 'bitclout_add_admin_menu' );
+add_action( 'admin_init', 'bitclout_settings_init' );
 
 
-function bitclout-wp-lite_add_admin_menu(  ) { 
+function bitclout_add_admin_menu(  ) { 
 
-	add_options_page( 'BitClout Wordpress Plugin Lite', 'BitClout Wordpress Plugin Lite', 'manage_options', 'bitclout_wordpress_plugin_lite', 'bitclout-wp-lite_options_page' );
+	add_options_page( 'BitClout Wordpress Plugin Lite', 'BitClout Wordpress Plugin Lite', 'manage_options', 'bitclout_wordpress_plugin_lite', 'bitclout_options_page' );
 
 }
 
 
-function bitclout-wp-lite_settings_init(  ) { 
+function bitclout_settings_init(  ) { 
 
-	register_setting( 'pluginPage', 'bitclout-wp-lite_settings' );
+	register_setting( 'pluginPage', 'bitclout_settings' );
 
 	add_settings_section(
-		'bitclout-wp-lite_pluginPage_section', 
+		'bitclout_pluginPage_section', 
 		__( 'Your section description', 'tier-settings' ), 
-		'bitclout-wp-lite_settings_section_callback', 
+		'bitclout_settings_section_callback', 
 		'pluginPage'
 	);
 
 	add_settings_field( 
-		'bitclout-wp-lite_text_field_0', 
+		'bitclout_text_field_0', 
 		__( 'Settings field description', 'tier-settings' ), 
-		'bitclout-wp-lite_text_field_0_render', 
+		'bitclout_text_field_0_render', 
 		'pluginPage', 
-		'bitclout-wp-lite_pluginPage_section' 
+		'bitclout_pluginPage_section' 
 	);
 
 	add_settings_field( 
-		'bitclout-wp-lite_textarea_field_1', 
+		'bitclout_textarea_field_1', 
 		__( 'Settings field description', 'tier-settings' ), 
-		'bitclout-wp-lite_textarea_field_1_render', 
+		'bitclout_textarea_field_1_render', 
 		'pluginPage', 
-		'bitclout-wp-lite_pluginPage_section' 
+		'bitclout_pluginPage_section' 
 	);
 
 	add_settings_field( 
-		'bitclout-wp-lite_text_field_2', 
+		'bitclout_text_field_2', 
 		__( 'Settings field description', 'tier-settings' ), 
-		'bitclout-wp-lite_text_field_2_render', 
+		'bitclout_text_field_2_render', 
 		'pluginPage', 
-		'bitclout-wp-lite_pluginPage_section' 
+		'bitclout_pluginPage_section' 
 	);
 
 	add_settings_field( 
-		'bitclout-wp-lite_textarea_field_3', 
+		'bitclout_textarea_field_3', 
 		__( 'Settings field description', 'tier-settings' ), 
-		'bitclout-wp-lite_textarea_field_3_render', 
+		'bitclout_textarea_field_3_render', 
 		'pluginPage', 
-		'bitclout-wp-lite_pluginPage_section' 
+		'bitclout_pluginPage_section' 
 	);
 
 	add_settings_field( 
-		'bitclout-wp-lite_text_field_4', 
+		'bitclout_text_field_4', 
 		__( 'Settings field description', 'tier-settings' ), 
-		'bitclout-wp-lite_text_field_4_render', 
+		'bitclout_text_field_4_render', 
 		'pluginPage', 
-		'bitclout-wp-lite_pluginPage_section' 
+		'bitclout_pluginPage_section' 
 	);
 
 	add_settings_field( 
-		'bitclout-wp-lite_textarea_field_5', 
+		'bitclout_textarea_field_5', 
 		__( 'Settings field description', 'tier-settings' ), 
-		'bitclout-wp-lite_textarea_field_5_render', 
+		'bitclout_textarea_field_5_render', 
 		'pluginPage', 
-		'bitclout-wp-lite_pluginPage_section' 
+		'bitclout_pluginPage_section' 
 	);
 
 
 }
 
 
-function bitclout-wp-lite_text_field_0_render(  ) { 
+function bitclout_text_field_0_render(  ) { 
 
-	$options = get_option( 'bitclout-wp-lite_settings' );
+	$options = get_option( 'bitclout_settings' );
 	?>
-	<input type='text' name='bitclout-wp-lite_settings[bitclout-wp-lite_text_field_0]' value='<?php echo $options['bitclout-wp-lite_text_field_0']; ?>'>
+	<input type='text' name='bitclout_settings[bitclout_text_field_0]' value='<?php echo $options['bitclout_text_field_0']; ?>'>
 	<?php
 
 }
 
 
-function bitclout-wp-lite_textarea_field_1_render(  ) { 
+function bitclout_textarea_field_1_render(  ) { 
 
-	$options = get_option( 'bitclout-wp-lite_settings' );
+	$options = get_option( 'bitclout_settings' );
 	?>
-	<textarea cols='40' rows='5' name='bitclout-wp-lite_settings[bitclout-wp-lite_textarea_field_1]'> 
-		<?php echo $options['bitclout-wp-lite_textarea_field_1']; ?>
+	<textarea cols='40' rows='5' name='bitclout_settings[bitclout_textarea_field_1]'> 
+		<?php echo $options['bitclout_textarea_field_1']; ?>
  	</textarea>
 	<?php
 
 }
 
 
-function bitclout-wp-lite_text_field_2_render(  ) { 
+function bitclout_text_field_2_render(  ) { 
 
-	$options = get_option( 'bitclout-wp-lite_settings' );
+	$options = get_option( 'bitclout_settings' );
 	?>
-	<input type='text' name='bitclout-wp-lite_settings[bitclout-wp-lite_text_field_2]' value='<?php echo $options['bitclout-wp-lite_text_field_2']; ?>'>
+	<input type='text' name='bitclout_settings[bitclout_text_field_2]' value='<?php echo $options['bitclout_text_field_2']; ?>'>
 	<?php
 
 }
 
 
-function bitclout-wp-lite_textarea_field_3_render(  ) { 
+function bitclout_textarea_field_3_render(  ) { 
 
-	$options = get_option( 'bitclout-wp-lite_settings' );
+	$options = get_option( 'bitclout_settings' );
 	?>
-	<textarea cols='40' rows='5' name='bitclout-wp-lite_settings[bitclout-wp-lite_textarea_field_3]'> 
-		<?php echo $options['bitclout-wp-lite_textarea_field_3']; ?>
+	<textarea cols='40' rows='5' name='bitclout_settings[bitclout_textarea_field_3]'> 
+		<?php echo $options['bitclout_textarea_field_3']; ?>
  	</textarea>
 	<?php
 
 }
 
 
-function bitclout-wp-lite_text_field_4_render(  ) { 
+function bitclout_text_field_4_render(  ) { 
 
-	$options = get_option( 'bitclout-wp-lite_settings' );
+	$options = get_option( 'bitclout_settings' );
 	?>
-	<input type='text' name='bitclout-wp-lite_settings[bitclout-wp-lite_text_field_4]' value='<?php echo $options['bitclout-wp-lite_text_field_4']; ?>'>
+	<input type='text' name='bitclout_settings[bitclout_text_field_4]' value='<?php echo $options['bitclout_text_field_4']; ?>'>
 	<?php
 
 }
 
 
-function bitclout-wp-lite_textarea_field_5_render(  ) { 
+function bitclout_textarea_field_5_render(  ) { 
 
-	$options = get_option( 'bitclout-wp-lite_settings' );
+	$options = get_option( 'bitclout_settings' );
 	?>
-	<textarea cols='40' rows='5' name='bitclout-wp-lite_settings[bitclout-wp-lite_textarea_field_5]'> 
-		<?php echo $options['bitclout-wp-lite_textarea_field_5']; ?>
+	<textarea cols='40' rows='5' name='bitclout_settings[bitclout_textarea_field_5]'> 
+		<?php echo $options['bitclout_textarea_field_5']; ?>
  	</textarea>
 	<?php
 
 }
 
 
-function bitclout-wp-lite_settings_section_callback(  ) { 
+function bitclout_settings_section_callback(  ) { 
 
 	echo __( 'This section description', 'tier-settings' );
 
 }
 
 
-function bitclout-wp-lite_options_page(  ) { 
+function bitclout_options_page(  ) { 
 
 		?>
 		<form action='options.php' method='post'>
