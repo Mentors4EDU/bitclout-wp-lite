2be50c09bf45d715a01d30048dd61d171fe0d6e3
Andrew Kamal <kamalandrew55@gmail.com>
2021-08-30 22:33:00 -0400

    Create bitclout-wp-lite.php

---
+++ bitclout-wp-lite.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: BitClout WP Lite Plugin
+ * Plugin URI: http://www.bitclout.com/u/AMKN
+ * Description: Minimalist creator coin tiers list for BitClout creators
+ * Version: 1.0
+ * Author: Andrew Kamal
+ * Author URI: http://www.bitclout.com/u/AMKN
+ */
