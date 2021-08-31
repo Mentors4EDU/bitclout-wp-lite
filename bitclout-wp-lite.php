63b6f80b9643b402e6614045afe5cdbd85993b6f
Andrew Kamal <kamalandrew55@gmail.com>
2021-08-30 22:50:07 -0400

    Create bitclout-wp-lite.php

---
+++ bitclout-wp-lite.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: BitClout WP Lite Plugin
+ * Plugin URI: http://www.peer-social.com
+ * Description: Minimalist creator coin tiers list for BitClout creators
+ * Version: 1.0
+ * Author: Andrew Kamal
+ * Author URI: http://www.peer-social.com
+ */
+include 'options.php';
+?>
